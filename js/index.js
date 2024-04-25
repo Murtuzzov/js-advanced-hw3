d936cdf55f6b9d51acd21c1c2c29d9c004443c39
murtuzzov <murtuzzov>
2024-04-26 03:39:13 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+//ЗАДАЧА №1
+// Создать html-страницу для ввода имени пользователя.
+// Необходимо проверять каждый символ, который вводит пользователь. Если он ввел цифру, то не отображать ее в input.
+
+// const container = document.querySelector(".container");
+// const form = document.querySelector(".form");
+// const display = document.querySelector('[data-action="display"]');
+
+// let filteredInput = "";
+// display.addEventListener("input", (e) => {
+//   if (!isNaN(Number(e.data))) {
+//     display.value = filteredInput;
+//     return;
+//   }
+//   filteredInput += e.data;
+// });
+
+//_________________________________________________________
+
+// Задание №2
+// Создать html-страницу с кнопкой Открыть и модальным
+// окном. На модальном окне должен быть текст и кнопка Закрыть.
+// Изначально модальное окно не отображается. При клике на
+// кнопку Открыть появляется модальное окно, на кнопку Закрыть –
+// исчезает.
+
+// const button = document.querySelector(".btn");
+// let modalWindow = document.querySelector(".modal-window");
+// let modalButton = document.querySelector(".modal-btn");
+
+// button.addEventListener("click", () => {
+//   modalWindow.style.display = "block";
+//   container.style.backgroundColor = "#f7f7f7";
+// });
+
+// modalButton.addEventListener("click", () => {
+//   modalWindow.style.display = "none";
+//   container.style.backgroundColor = "white";
+// });
